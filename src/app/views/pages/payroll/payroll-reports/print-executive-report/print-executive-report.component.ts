b37776bfd28d58cf95a82c8a1a8de41c12a65d87
khubaib waheed <khubaib.waheed@ratechnologies.biz>
2022-11-08 17:43:32 +0500

    payroll executive report print page

--- src/app/views/pages/payroll/payroll-reports/print-executive-report/print-executive-report.component.ts
+++ src/app/views/pages/payroll/payroll-reports/print-executive-report/print-executive-report.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Injector, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppComponentBase } from 'src/app/views/shared/app-component-base';
+import { AppConst } from 'src/app/views/shared/AppConst';
+import { PayrollReportsService } from '../service/payroll-reports.service';
 
 @Component({
   selector: 'kt-print-executive-report',
   templateUrl: './print-executive-report.component.html',
   styleUrls: ['./print-executive-report.component.scss']
 })
-export class PrintExecutiveReportComponent implements OnInit {
+export class PrintExecutiveReportComponent extends AppComponentBase implements OnInit {
 
-  constructor() { }
+  docType = AppConst.DocTypeValue
+  isLoading = true;
+  rowData: Map<any, any> = new Map<any, any>()
+  account: any;
+  businessPartner: any;
+  from: any;
+  to: any;
+  campus: any;
+  store: any;
+
+
+  constructor(
+    injector: Injector,
+    private activatedRoute: ActivatedRoute,
+    private payrollReportService: PayrollReportsService,
+    private cdRef: ChangeDetectorRef
+  ) {
+    super(injector);
+  }
 
   ngOnInit(): void {
+    this.payrollReportService.currentPayrollExecutivePrintData.subscribe((res) => {
+      if (res.length > 0) {
+        this.rowData = this.groupBy(res, item => item.accountName);
+        this.isLoading = false;
+      }
+    });
+
+    this.activatedRoute.queryParamMap.subscribe((param) => {
+      this.businessPartner = param.get('businessPartner');;
+      this.account = param.get('account');
+      this.from = param.get('from');
+      this.to = param.get('to');
+      this.campus = param.get('campus');
+      this.store = param.get('store');
+    });
   }
 
 }
+
+
+
+
