a15d405c8ee174aa3a97db77bbf5338c29d51677
ali khan <Ali.moonis@ratechnologies.net>
2023-01-20 11:07:29 +0500

    Depreciation model from font end with out api integration.

--- src/app/views/pages/fixed-asset/depreciation-model/service/depreciation-method.service.ts
+++ src/app/views/pages/fixed-asset/depreciation-model/service/depreciation-method.service.ts
@@ -12,7 +12,7 @@ import { AppConst } from 'src/app/views/shared/AppConst';
 })
 export class DepreciationMethodService extends AppServiceBase {
 
-    baseUrl = AppConst.remoteServiceBaseUrl + 'DepreciationModel';
+    baseUrl = AppConst.remoteServiceBaseUrl + 'Depreciation';
     
     constructor(private httpClient: HttpClient, injector: Injector) { super(injector) }
 
