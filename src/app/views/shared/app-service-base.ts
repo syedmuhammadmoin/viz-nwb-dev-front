e11d8e40f3f3f372426dccc8d38ab460d1f1469a
Waleed Siddique <waleed.siddique@ratechnologies.net>
2024-05-10 18:11:58 +0500

    filter issues has beeen resolved procurement only

--- src/app/views/shared/app-service-base.ts
+++ src/app/views/shared/app-service-base.ts
@@ -30,6 +30,13 @@ export abstract class AppServiceBase {
     httpParams = httpParams.append('Account', params?.filterModel?.accountTitle?.filter || '');
     
     httpParams = httpParams.append('docNo', (params?.filterModel?.docNo?.filter || params?.filterModel?.cnic?.filter) ||'');
+    httpParams = httpParams.append('title', (params?.filterModel?.title?.filter || "") || '');
+    httpParams = httpParams.append('category', (params?.filterModel?.category?.filter || "") || '');
+    httpParams = httpParams.append('description', (params?.filterModel?.description?.filter || "") || '');
+    httpParams = httpParams.append('unitOfMeasurement', (params?.filterModel?.unitOfMeasurement?.filter || "") || '');
+    httpParams = httpParams.append('availableQuantity', (params?.filterModel?.availableQuantity?.filter || "") || '');
+    httpParams = httpParams.append('reservedQuantity', (params?.filterModel?.reservedQuantity?.filter || "") || '');
+    httpParams = httpParams.append('tendorInquiryNumber', (params?.filterModel?.tendorInquiryNumber?.filter || "") || '');
     httpParams = httpParams.append('businessPartner', (params?.filterModel?.businessPartnerName?.filter || params?.filterModel?.businessPartner?.filter || params?.filterModel?.customerName?.filter || params?.filterModel?.vendorName?.filter ||  params?.filterModel?.employee?.filter ||  params?.filterModel?.employeeName?.filter || params?.filterModel?.bankName?.filter) || '');
     httpParams = httpParams.append('department', (params?.filterModel?.departmentName?.filter || params?.filterModel?.department?.filter) || '');
     httpParams = httpParams.append('designation', (params?.filterModel?.designationName?.filter || params?.filterModel?.designation?.filter) || '');
@@ -38,6 +45,7 @@ export abstract class AppServiceBase {
     httpParams = httpParams.append('itemCode', (params?.filterModel?.itemCode?.filter) || '' );
     httpParams = httpParams.append('docDate', date || '');
     httpParams = httpParams.append('dueDate', this.dateHelperService.transformDate(params?.filterModel?.dueDate?.dateFrom, 'MM/d/y') || '');
+    httpParams = httpParams.append('callForQuotationDate', this.dateHelperService.transformDate(params?.filterModel?.callForQuotationDate?.dateFrom, 'MM/d/y') || '');
     httpParams = httpParams.append('state', status);
     
     return httpParams
